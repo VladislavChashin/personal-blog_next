e507ef23a2bc2e0c004a89104c5afa034cf4f954
vladislav <chasinvlad39@gmail.com>
2023-03-23 18:22:39 +0300

    swaper mobile and travis aplicae

--- pages/components/posts/posts.tsx
+++ pages/components/posts/posts.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/dist/client/image'
 
 
-export function Posts(){
+export function Posts(props:any){
     return(
         <div className="post">
             <div className="post_content">
                 <div className="picture">
-                    <Image src='/posts/img/Phone.png' width={600} height={245} alt="" />
+                    <Image src={props.href} width={600} height={245} alt="" />
                 </div>
                 <div className="description">
-                    <h2>Как писать код быстро и безболезненно?</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.</p>
+                    <h2>{props.text}</h2>
+                    <p>{props.txt}</p>
                 </div>
                 <div className="description_bottom">
-                    <p>21.06.2020</p>
+                    <p>{props.date}</p>
                     <p>создание сайтов</p>
                     <a href="/post_one">читать</a>
                 </div>
--- pages/components/stories/stories.tsx
+++ pages/components/stories/stories.tsx
@@ -1,26 +1,63 @@
 import Image from 'next/dist/client/image'
 import { useDispatch } from 'react-redux'
 import { increment } from './slice'
+import {useSwipeable} from 'react-swipeable'
+import { useState } from 'react'
+import { transform } from 'typescript'
 
 export function Stories(){
     const dispatch = useDispatch()
+    const [activeStries, setActiveStories] = useState(0)
 
+    const updateIndex = (counter:number) => {
+        if (activeStries > 0){
+            setActiveStories(-1)
+        }else{
+           setActiveStories(counter)
+        }
+    }
+    const remuveIndex = (counter:number) => {
+        if (activeStries < 0){
+            setActiveStories(1)
+        }else{
+           setActiveStories(counter)
+        }
+    }
+
+    const hendlers = useSwipeable({
+        onSwipedLeft: () => remuveIndex(activeStries - 1),
+        onSwipedRight: () => updateIndex(activeStries + 1)
+    })
     return(
-        <div className="stories">
-            <div className="stories_content">
+        <div {...hendlers} className="stories" >
+            <div className="stories_content" style={{transform: `translateX(${activeStries * 50}%)`}}>
                 <div className="stories_post" onClick={() => {dispatch(increment())}}>
                     <p>Отдыхаю на природе</p>
                     <div className="image">
-                        <Image src='/stories/img/Rectangle1.jpg' width={140} height={195} alt="" />
+                        <Image src='/stories/img/Rectangle4.jpg' width={140} height={195} alt="" />
                     </div>
-                    <p>21.09.2023</p>
+                    <p>21.01.2023</p>
                 </div>
                 <div className="stories_post" onClick={() => {dispatch(increment())}}>
-                    <p>Отдыхаю на природе</p>
+                    <p>Заканчиваю сложный проект</p>
                     <div className="image">
                         <Image src='/stories/img/Rectangle1.jpg' width={140} height={195} alt="" />
                     </div>
-                    <p>21.09.2023</p>
+                    <p>9.12.2022</p>
+                </div>
+                <div className="stories_post" onClick={() => {dispatch(increment())}}>
+                    <p>Переехал в новую квартиру</p>
+                    <div className="image">
+                        <Image src='/stories/img/Rectangle2.jpg' width={140} height={195} alt="" />
+                    </div>
+                    <p>19.10.2022</p>
+                </div>
+                <div className="stories_post" onClick={() => {dispatch(increment())}}>
+                    <p>Осень пришла!</p>
+                    <div className="image">
+                        <Image src='/stories/img/Rectangle3.jpg' width={140} height={195} alt="" />
+                    </div>
+                    <p>21.09.2022</p>
                 </div>
             </div>
         </div>
